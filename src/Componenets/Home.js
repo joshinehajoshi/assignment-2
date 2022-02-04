29265704bdad17e5487dfc16fcc7f62efd62dba9
Neha Joshi <nehajoshigkp@gmail.com>
2022-02-04 13:01:00 +0530

    api call added

--- src/Componenets/Home.js
+++ src/Componenets/Home.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../styling/home.css';
-import { Navbar, Container, NavDropdown, Nav, FormControl } from 'react-bootstrap'
+import { Navbar, Container, NavDropdown, Nav, FormControl, Button } from 'react-bootstrap'
 
 function Home() {
+
+    const [item, setItem] = useState([]);
+    const [status, setStatus] = useState(false);
+    
+    const apiCall = () => {
+        fetch('http://www.mocky.io/v2/5ba8efb23100007200c2750c')
+        .then(response => response.json())
+        .then(data => setItem(data));
+        console.log(item)
+    }
+
+
+    useEffect(() => {
+       apiCall()
+    },[])
+
+
+
+
   return <div>
             <Navbar bg="light" expand="lg">
             <Container>
@@ -31,6 +50,11 @@ function Home() {
                 </Navbar.Collapse>
             </Container>
             </Navbar>
+            <div>
+                <Container>
+                <Button onClick={apiCall}>Okay</Button>
+                </Container>
+            </div>
         </div>;
 }
 
